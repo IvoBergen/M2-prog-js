f48c0151497722964e412baf854dff9c103c9a66
Ivo <34885@ma-web.nl>
2022-12-21 12:34:48 +0100

    Dobbelsteen aangepast

--- dobbelsteen/app.js
+++ dobbelsteen/app.js
@@ -1,30 +1,35 @@
 class App{
     runApplication(){
-        console.log("Hello world");
-        let stat1 = this.rollStat();
-        console.log(stat1);
-        let headerEl1 = document.getElementById('s1');
-        headerEl1.innerHTML = stat1;
-        let stat2 = this.rollStat();
-        console.log(stat2);
-        let headerEl2 = document.getElementById('s2');
-        headerEl2.innerHTML = stat2;
-        let stat3 = this.rollStat();
-        console.log(stat3);
-        let headerEl3 = document.getElementById('s3');
-        headerEl3.innerHTML = stat3;
-        let stat4 = this.rollStat();
-        console.log(stat4);
-        let headerEl4 = document.getElementById('s4');
-        headerEl4.innerHTML = stat4;
-        let stat5 = this.rollStat();
-        console.log(stat5);
-        let headerEl5 = document.getElementById('s5');
-        headerEl5.innerHTML = stat5;
-        let stat6 = this.rollStat();
-        console.log(stat6);  
-        let headerEl6 = document.getElementById('s6');
-        headerEl6.innerHTML = stat6;
+        
+        let button = document.getElementById('rerollButton');
+        button.addEventListener('click', ()=>{
+            let stat1 = this.rollStat();
+            console.log(stat1);
+            let headerEl1 = document.getElementById('s1');
+            headerEl1.innerHTML = stat1;
+            let stat2 = this.rollStat();
+            console.log(stat2);
+            let headerEl2 = document.getElementById('s2');
+            headerEl2.innerHTML = stat2;
+            let stat3 = this.rollStat();
+            console.log(stat3);
+            let headerEl3 = document.getElementById('s3');
+            headerEl3.innerHTML = stat3;
+            let stat4 = this.rollStat();
+            console.log(stat4);
+            let headerEl4 = document.getElementById('s4');
+            headerEl4.innerHTML = stat4;
+            let stat5 = this.rollStat();
+            console.log(stat5);
+            let headerEl5 = document.getElementById('s5');
+            headerEl5.innerHTML = stat5;
+            let stat6 = this.rollStat();
+            console.log(stat6);  
+            let headerEl6 = document.getElementById('s6');
+            headerEl6.innerHTML = stat6;
+        });
+
+        
     }
 
     rollDice(){
